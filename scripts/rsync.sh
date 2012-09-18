5bcb90806cb6d8a1deecccf94eea2da75433f3d7
Igor Hlina <srigi@srigi.sk>
2012-09-18 09:33:02 +0200

    authentication via SSH

--- scripts/rsync.sh
+++ scripts/rsync.sh
@@ -2,8 +2,10 @@
 
 rsync -azv --delete --delete-excluded --delete-after \
 	--include ".htaccess" \
+	--filter "P .ssh" \
 	--filter "P log/*.log" \
 	--filter "P log/*.html" \
+	--exclude ".git" \
 	--exclude "log/*" \
 	--exclude "temp/*" \
 	--exclude "scripts" \
